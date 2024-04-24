d30a7df7118a7c07850d3320aa05f04d4a9e2b0a
ShreyaGaonkar <shreyagaonkar99@gmail.com>
2024-04-24 22:36:47 +0530

    Display song section

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -5,13 +5,66 @@ import styles from './App.css'
 import { useEffect, useState } from 'react'
 import { fetchTopAlbums, fetchNewAlbums, fetchSongs } from './api/api'
 import Section from './components/Section/Section.jsx'
+import FilterSection from './components/FilterSection/FilterSection'
 
 function App() {
 
   const [topAlbumSongs, setTopAlbumSongs] = useState([])
   const [newAlbumSongs, setNewAlbumSongs] = useState([])
+  const [filteredDataValues, setFilteredDataValues] = useState([''])
   const [toggle, setToggle] = useState(false)
   const [value, setValue] = useState(0);
+
+  const generateSongsData = (value) => {
+
+    let songData = newAlbumSongs[0].songs;
+
+    let key;
+    if (value === 0) {
+
+      setFilteredDataValues(songData)
+      return;
+    }
+
+    else if (value === 1) {
+      key = 'rock'
+
+    }
+
+    else if (value === 2) {
+      key = 'pop'
+    }
+
+    else if (value === 3) {
+      key = 'jazz'
+    }
+
+    else if (value === 4) {
+      key = 'blues'
+    }
+
+    const data = songData.filter((item) => {
+      return item.genre.key === key
+
+    })
+    setFilteredDataValues(data)
+
+  }
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue)
+
+    generateSongsData(newValue)
+
+  }
+  const handleToggle = () => {
+    setToggle(!toggle)
+  }
+
+  const filteredData = (val) => {
+    generateSongsData(val)
+    
+  }
 
   const generateTopAlbumSongs = async () => {
     try {
@@ -38,6 +91,20 @@ function App() {
   }
 
 
+  const generateFilterSongs = async () => {
+
+    try {
+      const newAlbumSongs = await fetchSongs()
+      setFilteredDataValues(newAlbumSongs);
+    }
+
+    catch (error) {
+      console.log(error)
+      return null 
+
+    }
+  }
+
   useEffect(() => {
     // eslint-disable-next-line
   }, [value])
@@ -46,6 +113,8 @@ function App() {
     
     generateTopAlbumSongs();
     generateNewAlbumSongs();
+    generateFilterSongs();
+    // setFilteredDataValues(newAlbumSongs);
 
   }, [])
   
@@ -57,6 +126,7 @@ function App() {
       <div className={styles.sectionWrapper}>
         <Section type='album' title='Top Albums' data={topAlbumSongs} />
         <Section type='album' title='New Albums' data={newAlbumSongs} />
+        <FilterSection data={newAlbumSongs} type='songFilter' title='Songs' filteredData={filteredData} filteredDataValues={filteredDataValues} value={value} handleChange={handleChange} handleToggle={handleToggle}/>
       </div>
     </>
   )
