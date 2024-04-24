2fd327c8f8283f8e61c50c7342f8b475dd230e2a
ShreyaGaonkar <shreyagaonkar99@gmail.com>
2024-04-24 20:51:51 +0530

    Added Hero Section

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,10 +1,12 @@
 import Navbar from './components/Navbar/Navbar.jsx'
+import Hero from './components/Hero/Hero.jsx'
 import './App.css';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   );
 }
